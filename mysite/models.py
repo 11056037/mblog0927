031e88dfe366c2931c6690d2f05232ab7e2b39aa
11056037 <11056037@ntub.edu.tw>
2023-10-04 15:57:48 +0800

    新增Post物件管理介面

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    #屬性
+    title=models.CharField(max_length=200) #models.CharField 指定資料型態
+    slug=models.CharField(max_length=200)
+    body=models.TextField()
+    pub_date=models.DateTimeField(auto_now_add=True) #(自動取得時間)
+    
+    class Mate:
+        ordering=('-pub_date', ) #欄位ordering
+    
+    #複寫
+    def __str__(self) -> str:
+        return self.title
