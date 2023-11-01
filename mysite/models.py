c5029c2dea69f100c7db34b4c95b983f0625e20a
11056037 <11056037@ntub.edu.tw>
2023-11-01 16:36:14 +0800

    嘎油

--- mysite/models.py
+++ mysite/models.py
@@ -14,3 +14,16 @@ class Post(models.Model):
     #複寫
     def __str__(self) -> str:
         return self.title
+    
+class Product(models.Model):
+    SIZES = (
+        ('S', 'Smaill'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    sku = models.CharField(max_length=5)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    size = models.CharField(max_length=1, choices=SIZES)
+    result =models.BooleanField()
+   
